4a7cb2811a0dc37542a28fea84d3154d9440a36d
Raveen K Sharma <raveen08@gmail.com>
2021-02-08 23:19:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ class Bird {
     this.sex = sex;
   }
     speak(){ if(this.sex == "male"){
-    return `It's me! ${this.name} the parrot!`}
+    return `It's me! ${this.name}, the parrot!`}
     return `${this.name} says squawk!`
   }
 }
